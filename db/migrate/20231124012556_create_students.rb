3d42d86bdeccc899e6b9781bcc3df1f85dce3f5b
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-24 05:07:04 +0200

    made first db migration

--- db/migrate/20231124012556_create_students.rb
+++ db/migrate/20231124012556_create_students.rb
@@ -1,7 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[7.1]
   def change
     create_table :students do |t|
-
+      t.string :name
+      t.integer :age
+      t.boolean :dorm
+      t.string :language
+      t.boolean :teacher
+      t.string :university
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Student.create(
+  name: 'Liz Valley',
+  age: 20,
+  dorm: true,
+  language: 'Python',
+  teacher: false,
+  university: 'MNK University'
+)
