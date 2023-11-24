ff8a26c4930aeb22443ade029ff21e383d47fa30
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-24 06:42:18 +0200

    added actions to controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,24 @@
 class StudentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    if student.save
+      session[:student_id] = @student.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+  private
+  def student_params
+    params.require(:student).permit(:name,:age,:dorm,:language,:teacher,:university)
   end
 end
