325c78a4d203dab5c79155b97e65911ba443451e
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-27 11:31:08 +0200

    added styles

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new(student_params)
     if @student.save
-      redirect_to '/index'
+      redirect_to '/'
     else
       render '/new'
     end
